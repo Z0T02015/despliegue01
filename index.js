21bb9e4a296f9390a61953330e4832b395f880f7
Z0T02015 <cristhian.soto@tecsup.edu.pe>
2026-05-04 11:12:28 -0500

    actualizacion

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+require('dotenv').config()
+const http = require('http')
 
+function requestController(req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(`
+        <h1>🚀 Backend funcionando</h1>
+        <p>Servidor activo correctamente</p>
+    `)
+}
+
+const server = http.createServer(requestController)
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`)
+})
